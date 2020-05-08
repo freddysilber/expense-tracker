b9ae40ea46ead587aac3b8460863d477e8e411ac
freddysilber <freddy.silber@cloudcoach.com>
2020-05-08 16:37:55 -0600

    fixing cors

--- expense-tracker-frontend/src/App.js
+++ expense-tracker-frontend/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+export default class App extends React.Component {
+  componentDidMount() {
+    fetch('http://localhost:3000/users')
+      .then(response => console.log(response.json()))
+      .catch(error => console.log(error))
+  }
+  render() {
+    return (
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    )
+  }
 }
-
-export default App;
