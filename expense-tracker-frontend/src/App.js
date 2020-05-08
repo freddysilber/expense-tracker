20dd351182da1bc87ca430b7271ba59e41219c25
freddysilber <freddy.silber@cloudcoach.com>
2020-05-08 16:42:39 -0600

    formatting

--- expense-tracker-frontend/src/App.js
+++ expense-tracker-frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import Home from './components/Home'
 
 export default class App extends React.Component {
+
   componentDidMount() {
     fetch('http://localhost:3000/users')
       .then(response => console.log(response.json()))
       .catch(error => console.log(error))
   }
+
   render() {
     return (
       <Home />
